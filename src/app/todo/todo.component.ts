44534266206282ade685a1a672aedae34a320c8b
Przemysław Nowakowsk <przenowako@gmail.com>
2022-07-11 12:30:28 +0200

    Test

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger,animate,style,transition,keyframes } from '@angular/animations';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  animations:[
+     trigger("moveInLeft",[
+        transition("void=> *",[style({transform:"translateX(300px)"}),
+          animate(200,keyframes([
+           style({transform:"translateX(300px)"}),
+           style({transform:"translateX(0)"})
+            ]))]),
+            transition("*=>void",[style({transform:"translateX(0px)"}),
+          animate(100,keyframes([
+           style({transform:"translateX(0px)"}),
+           style({transform:"translateX(300px)"})
+            ]))])
+     	])
+    ]
+})
+export class TodoComponent implements OnInit {
+  todoArray: any[] = [];
+    todo: any;
+    //todoForm: new FormGroup()
+
+     addTodo(value: any): void {
+      if(value!==""){
+       this.todoArray.push(value);
+      //console.log(this.todos)
+    }else{
+      alert('Field required **')
+    }
+    }
+
+    /*delete item*/
+    deleteItem(todo: any){
+    	for(let i=0 ;i<= this.todoArray.length ;i++){
+    		if(todo== this.todoArray[i]){
+    			this.todoArray.splice(i,1)
+    		}
+    	}
+    }
+
+    // submit Form
+    todoSubmit(value:any){
+       if(value!==""){
+      this.todoArray.push(value)
+      }else{
+        alert('Wprowadz dane!')
+      }
+
+    }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
